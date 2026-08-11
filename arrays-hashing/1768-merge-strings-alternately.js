c4c4cf6e4a4026340bf11fda624dd91fb8a06db1
Paul Perrin <247144100+paulperrin-stack@users.noreply.github.com>
2026-08-11 16:58:17 +0800

    1768: merge strings alternately

---
+++ arrays-hashing/1768-merge-strings-alternately.js
@@ -0,0 +1,17 @@
+const word1 = "dog";
+const word2 = "cat";
+
+function mergeAlternately(word1, word2) {
+    let result = "";
+    let i = 0;
+
+    while (i < word1.length || i < word2.length) {
+        if (i < word1.length) result += word1[i];
+        if (i < word2.length) result += word2[i];
+        i++
+    }
+
+    return result;
+}
+
+console.log(mergeAlternately(word1, word2));
